3ca6735cd92b1dc1c4cda547d5ea51be5544c3a7
Asaolu James <asaolujtomi@gmail.com>
2025-02-28 17:44:55 +0100

    feat: abi integration for the creator dashboard

--- components/explore/hero-section.tsx
+++ components/explore/hero-section.tsx
@@ -6,14 +6,14 @@ import secondImage from "@/assets/explore-2.png";
 
 const Herosection = () => {
   return (
-    <div className="flex justify-between p-10 border-y">
+    <div className="grid lg:flex lg:justify-between p-10 border-y">
       <div className="space-y-5">
         <h2 className=" font-bold text-2xl">Marketing your NFTs</h2>
         <p className="">Strategies for Success in a Competitive Space</p>
       </div>
 
-      <Image src={firstImage} alt="" />
-      <Image src={secondImage} alt="" />
+      <Image src={firstImage} alt="" className="hidden lg:block mt-4 lg:mt-0" />
+      <Image src={secondImage} alt="" className="mt-6 lg:mt-0" />
     </div>
   );
 };
